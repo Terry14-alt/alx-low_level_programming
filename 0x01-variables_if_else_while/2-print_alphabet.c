d9e37dd8c5266d042e1c4bbf3b9b01b6ed76c4ae
Terry14-alt <teaziaba@st.ug.edu.gh>
2022-10-17 20:30:10 +0000

    alphABET

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabets in lower case
+ *
+ * Return: Always 0 successfully
+ */
+int main(void)
+{
+	char lower_case;
+
+	for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
+	{
+		putchar(lower_case);
+	}
+	putchar('\n');
+	return (0);
+}
