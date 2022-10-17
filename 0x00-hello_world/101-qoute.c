ad926c2df8f7de21c039b433df25c5f18266e8e0
Terry14-alt <teaziaba@st.ug.edu.gh>
2022-10-17 17:58:30 +0000

    first c code, print on standard output

---
+++ 0x00-hello_world/101-qoute.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ *
+ * return - This program returns zero
+ */
+int main(void)
+{
+	putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
