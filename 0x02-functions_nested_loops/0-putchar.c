928925fd4fd77c5d78e3d3e99ffe117314ed35f4
Terry14-alt <teaziaba@st.ug.edu.gh>
2022-10-20 01:19:20 +0000

    correction of 0-putchar file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,13 +6,14 @@
  */
 int main(void)
 {
-	char pr = "_putchar";
-
-	while (pr)
-	{
-		_putchar(pr);
-		pr++;
-	}
+	_putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
 	_putchar('\n');
 
 	return (0);
