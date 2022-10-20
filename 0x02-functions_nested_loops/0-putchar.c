692122a2f2eb34e11c04f23563351bf340cc3476
Terry14-alt <teaziaba@st.ug.edu.gh>
2022-10-20 00:00:35 +0000

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <main.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *pr = "_putchar";
+
+	while (*pr)
+	{
+		_putchar(*pr);
+		pr++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
