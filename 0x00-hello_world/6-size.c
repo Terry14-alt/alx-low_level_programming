f8028d8b036b73152d24a0a9ce273ae7342bdb87
Terry14-alt <terryaziab@gmail.com>
2023-10-10 04:54:41 +0000

    Size of program

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int main(void)
 	long long d;
 	float e;
 
-	printf("The size of a char is: %lu.\n", (unsigned long)sizeof(a));
-	printf("the size of an int is %lu.\n", (unsigned long)sizeof(b));
-	printf("The size of a long is %lu.\n", (unsigned long)sizeof(c));
-	printf("The size of a long long is %lu.\n", (unsigned long)sizeof(d));
-	printf("The size of a float is %lu.\n", (unsigned long)sizeof(e));
+	printf("Size of a char is: %lu. byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of an int is %lu. byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long is %lu. byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a long long is %lu. byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of a float is %lu. byte(s)\n", (unsigned long)sizeof(e));
 	return (0);
 }
